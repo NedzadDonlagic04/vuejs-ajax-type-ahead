d5ee2ccc463427f307484f67f558f125cc7614fa
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-15 18:31:49 +0200

    Added input event listener
    So that it will send the gotten list of places
    to the app component which is it's parent.

--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -1,12 +1,62 @@
+<script>
+export default {
+    data()
+    {
+        return {
+            link: 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+        };
+    },
+    emits: ['getPlaces'],
+    methods: {
+        async getInfo(event)
+        {   
+            const value = event.target.value.toLowerCase();
+
+            await fetch(this.link)
+                .then( response => {
+                    if(response.ok)
+                    {
+                        return response.json();
+                    }
+                    return Promise.reject();
+                })
+                .then( data => {
+                    const arr = data.filter( item => {
+                                        const city = item.city.toLowerCase();
+                                        const state = item.state.toLowerCase();
+
+                                        return city.includes(value) || state.includes(value);
+                                    })
+                                    .sort( (a, b) => {
+                                        return (a.population - b.population)? 1 : -1;
+                                    })
+                                    .map( item => (
+                                        { 
+                                            city: item.city,
+                                            state: item.state,
+                                            population: item.population
+                                        }
+                                    ));
+
+                    this.$emit('getPlaces', arr);
+                })
+                .catch( error => {
+                    console.log(`Error: ${error}`);
+                });
+        }
+    }
+};
+</script>
+
 <template>
     <div class="input-box">
-        <input type="text" placeholder="City or State">
+        <input type="text" placeholder="City or State" @input="getInfo">
     </div>
 </template>
 
 <style scoped>
     .input-box {
-        width: 20rem;
+        width: 22rem;
         height: 5rem;
         background-color: var(--bg-dark-white);
         border-radius: var(--border-rd);
