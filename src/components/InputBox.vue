628b68d937cda8f67c422d126dfb9595a9590a07
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-15 15:07:30 +0200

    Added InputBox component
    Used to take user input

---
+++ src/components/InputBox.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="input-box">
+        <input type="text" placeholder="City or State">
+    </div>
+</template>
+
+<style scoped>
+    .input-box {
+        width: 20rem;
+        height: 5rem;
+        background-color: var(--bg-dark-white);
+        border-radius: var(--border-rd);
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .input-box input {
+        width: 95%;
+        height: 80%;
+        font-size: 2rem;
+        text-align: center;
+        outline: none;
+        border: var(--border-rd) solid var(--border-white);
+        border-radius: var(--border-rd);
+    }
+</style>
