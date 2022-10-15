25ee03debc07e4222cbd00c07e44f7d319496992
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-15 19:50:58 +0200

    Added handle for the event as well as style

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,16 @@ export default {
     components: {
         InputBox
     },
+    data()
+    {
+        return {
+            places: []
+        };
+    }, 
     methods: {
         placesHandler(value)
         {
-            console.log(value);
+            this.places = value;
         }
     }
 };
@@ -16,5 +22,38 @@ export default {
 
 <template>
     <InputBox @get-places="placesHandler"/>
-    <div class="container"></div>
+    <div class="container" v-for="place of places">
+        <div class="names">
+            <p>{{place.city}}, {{place.state}}</p>
+        </div>
+        <div class="numbers">
+            <p>{{place.population}}</p>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    .container {
+        margin: 0 auto;
+        padding: .5rem;
+        width: 20rem;
+        height: 3.5rem;
+        background: linear-gradient(to top, var(--list-bg) 0%, var(--border-white) 100%);
+        border-bottom: 2px solid var(--list-bg);
+        box-shadow: 0px 0px 10px var(--list-shadow);
+        font-size: 1.2rem;
+
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .container:nth-child(odd) {
+        transform: perspective(100px) rotateX(3deg) translateY(-2px);
+    }
+
+    .container:nth-child(even) {
+        margin-top: -3px;
+        transform: perspective(100px) rotateX(-3deg);    
+    }
+</style>
