25a159e76698556ed053fc821fc46bf2a765235a
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-15 14:23:35 +0200

    Added vue file boilerplate

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data()
+    {
+        return {
+            msg: 'Hello world!'
+        };
+    }
+}
+</script>
+
+<template>
+    <p>{{msg}}</p>
+</template>
+
+<style scoped>
+    p {
+        color: red;
+    }
+</style>
